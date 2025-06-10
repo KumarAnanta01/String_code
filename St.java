3074a8167fec8231cb0b43611bdd493b255e67e8
KumarAnanta01 <Ananta.Kumar9096@gmail.com>
2025-06-11 03:35:28 +0530

    two string classes are added to git hub

--- St.java
+++ St.java
@@ -2,7 +2,19 @@ package StringP;
 
 public class St {
     public static void main(String[] args) {
+
         String str1= "java Program";
-        System.out.println(str1);
+
+        String str2= new String("JAVA");
+
+        char c[]={'D','I','P','U'};
+
+        String str3=new String(c);
+
+        byte b[]={65,66,67,68};
+
+        String str4=new String(b,2,2);
+
+        System.out.println(str4);
     }
 }
---
+++ St2.java
@@ -0,0 +1,11 @@
+package StringP;
+
+public class St2 {
+
+    public static void main(String[] args) {
+        String str1="java";
+      //  String str2="Java";
+        String str2=new String("java");
+        System.out.println(str1==str2);
+    }
+}
